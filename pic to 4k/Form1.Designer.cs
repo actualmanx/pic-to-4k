7f7b1af6c39757169cfd763896b1bb8e8d78c961
Kieran HIll <kieranhill@hotmail.co.uk>
2024-05-18 21:18:31 +0100

    Add project files.

---
+++ pic to 4k/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace pic_to_4k
+{
+    partial class PicResize
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PicResize));
+            this.inputbutton = new System.Windows.Forms.Button();
+            this.outputandsave = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.fileStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.widthtextBox1 = new System.Windows.Forms.TextBox();
+            this.heighttextBox1 = new System.Windows.Forms.TextBox();
+            this.width1 = new System.Windows.Forms.Label();
+            this.height1 = new System.Windows.Forms.Label();
+            this.formatComboBox = new System.Windows.Forms.ComboBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // inputbutton
+            // 
+            this.inputbutton.AllowDrop = true;
+            this.inputbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputbutton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.inputbutton.Location = new System.Drawing.Point(612, 165);
+            this.inputbutton.Name = "inputbutton";
+            this.inputbutton.Size = new System.Drawing.Size(123, 65);
+            this.inputbutton.TabIndex = 0;
+            this.inputbutton.Text = "Input File";
+            this.inputbutton.UseVisualStyleBackColor = true;
+            this.inputbutton.Click += new System.EventHandler(this.inputbutton_Click);
+            // 
+            // outputandsave
+            // 
+            this.outputandsave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputandsave.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.outputandsave.Location = new System.Drawing.Point(612, 259);
+            this.outputandsave.Name = "outputandsave";
+            this.outputandsave.Size = new System.Drawing.Size(122, 65);
+            this.outputandsave.TabIndex = 1;
+            this.outputandsave.Text = "Convert and Save";
+            this.outputandsave.UseVisualStyleBackColor = true;
+            this.outputandsave.Click += new System.EventHandler(this.outputandsave_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 412);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(778, 32);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // fileStripStatusLabel1
+            // 
+            this.fileStripStatusLabel1.Name = "fileStripStatusLabel1";
+            this.fileStripStatusLabel1.Size = new System.Drawing.Size(170, 25);
+            this.fileStripStatusLabel1.Text = "fileStripStatusLabel1";
+            // 
+            // widthtextBox1
+            // 
+            this.widthtextBox1.Location = new System.Drawing.Point(55, 232);
+            this.widthtextBox1.Name = "widthtextBox1";
+            this.widthtextBox1.Size = new System.Drawing.Size(100, 26);
+            this.widthtextBox1.TabIndex = 5;
+            this.widthtextBox1.Text = "3840";
+            // 
+            // heighttextBox1
+            // 
+            this.heighttextBox1.Location = new System.Drawing.Point(55, 277);
+            this.heighttextBox1.Name = "heighttextBox1";
+            this.heighttextBox1.Size = new System.Drawing.Size(100, 26);
+            this.heighttextBox1.TabIndex = 6;
+            this.heighttextBox1.Text = "2160";
+            // 
+            // width1
+            // 
+            this.width1.AutoSize = true;
+            this.width1.Location = new System.Drawing.Point(196, 238);
+            this.width1.Name = "width1";
+            this.width1.Size = new System.Drawing.Size(50, 20);
+            this.width1.TabIndex = 7;
+            this.width1.Text = "Width";
+            // 
+            // height1
+            // 
+            this.height1.AutoSize = true;
+            this.height1.Location = new System.Drawing.Point(196, 283);
+            this.height1.Name = "height1";
+            this.height1.Size = new System.Drawing.Size(56, 20);
+            this.height1.TabIndex = 8;
+            this.height1.Text = "Height";
+            // 
+            // formatComboBox
+            // 
+            this.formatComboBox.FormattingEnabled = true;
+            this.formatComboBox.Items.AddRange(new object[] {
+            "PNG",
+            "GIF",
+            "BMP",
+            "JPEG"});
+            this.formatComboBox.Location = new System.Drawing.Point(55, 48);
+            this.formatComboBox.Name = "formatComboBox";
+            this.formatComboBox.Size = new System.Drawing.Size(121, 28);
+            this.formatComboBox.TabIndex = 11;
+            this.formatComboBox.Text = "PNG";
+            // 
+            // PicResize
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(778, 444);
+            this.Controls.Add(this.formatComboBox);
+            this.Controls.Add(this.height1);
+            this.Controls.Add(this.width1);
+            this.Controls.Add(this.heighttextBox1);
+            this.Controls.Add(this.widthtextBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.outputandsave);
+            this.Controls.Add(this.inputbutton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "PicResize";
+            this.Text = "Pic Resize & Repeat";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button inputbutton;
+        private System.Windows.Forms.Button outputandsave;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel fileStripStatusLabel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox widthtextBox1;
+        private System.Windows.Forms.TextBox heighttextBox1;
+        private System.Windows.Forms.Label width1;
+        private System.Windows.Forms.Label height1;
+        private System.Windows.Forms.ComboBox formatComboBox;
+    }
+}
+
---
+++ pic to 4k/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Configuration;
+using System.Diagnostics;
+using System.Collections.Specialized;
+using Utility.ModifyRegistry;
+using Microsoft.Win32;
+
+namespace pic_to_4k
+{
+    public partial class PicResize : Form
+
+
+    {
+        
+
+
+        
+
+        public PicResize()
+        {
+            InitializeComponent();
+
+            openFileDialog1.Filter = "Image Files |*.JPEG;*.PNG;*.GIF;*.BMP;*.JPG";
+
+            fileStripStatusLabel1.Text = myRegistry.Read("File Location");
+            this.DragEnter += new DragEventHandler(Form_DragEnter);
+            this.DragDrop += new DragEventHandler(Form_DragDrop);
+        }
+
+
+        void Form_DragEnter(object sender, DragEventArgs e)
+        {
+            // Check if the Data format of the data can be accepted
+            // (we only accept file drops from Explorer, etc.)
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy; // Okay
+            else
+                e.Effect = DragDropEffects.None; // Unknown data, ignore it
+
+        }
+
+        void Form_DragDrop(object sender, DragEventArgs e)
+        {
+            // clear the label
+            fileStripStatusLabel1.Text = String.Empty;
+            // Extract the data from the DataObject-Container into a string list
+            string[] FileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            // Do something with the data...
+
+            // For example add all files into a simple label control:
+            foreach (string File in FileList)
+                this.fileStripStatusLabel1.Text += File;
+            myRegistry.Write("File Location", this.fileStripStatusLabel1.Text);
+        }
+        ModifyRegistry myRegistry = new ModifyRegistry();
+
+        private void inputbutton_Click(object sender, EventArgs e)
+        {
+            
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                myRegistry.Write("File Location", openFileDialog1.FileName);
+                fileStripStatusLabel1.Text = myRegistry.Read("File Location");
+            }
+        }
+
+        private void outputandsave_Click(object sender, EventArgs e)
+        {
+            string inputPath = fileStripStatusLabel1.Text;
+            string outputPath = fileStripStatusLabel1.Text + ("." + formatComboBox.Text).ToLower();
+            int outputWidth = int.Parse(widthtextBox1.Text);  // Convert string to int
+            int outputHeight = int.Parse(heighttextBox1.Text);  // Convert string to int
+
+            using (Image inputImage = Image.FromFile(inputPath))
+            using (Bitmap outputImage = new Bitmap(outputWidth, outputHeight))
+            using (Graphics graphics = Graphics.FromImage(outputImage))
+            {
+                for (int x = 0; x < outputWidth; x += inputImage.Width)
+                {
+                    for (int y = 0; y < outputHeight; y += inputImage.Height)
+                    {
+                        graphics.DrawImage(inputImage, new Point(x, y));
+                    }
+                }
+
+                // Determine the ImageFormat based on the selected item in the ComboBox
+                System.Drawing.Imaging.ImageFormat format;
+                switch (formatComboBox.SelectedItem.ToString())
+                {
+                    case "PNG":
+                        format = System.Drawing.Imaging.ImageFormat.Png;
+                        break;
+                    case "JPEG":
+                        format = System.Drawing.Imaging.ImageFormat.Jpeg;
+                        break;
+                    case "BMP":
+                        format = System.Drawing.Imaging.ImageFormat.Bmp;
+                        break;
+                    case "GIF":
+                        format = System.Drawing.Imaging.ImageFormat.Gif;
+                        break;
+                    default:
+                        throw new Exception("Unsupported image format.");
+                }
+
+                outputImage.Save(outputPath, format);
+            }
+        }
+        }
+    }
+
+    
+
